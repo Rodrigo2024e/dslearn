e19f9aecbc06e06d3a4e1d865bc0f3beba21d374
Rodrigo <lucianorodrigocarvalho@gmail.com>
2025-06-25 07:28:19 -0300

    fisrt commit

---
+++ src/test/java/com/smartprocess/dslearn/DslearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartprocess.dslearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
